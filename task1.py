16f9a201a08ea87cefa2737adc7d85a2c21fd42c
Edward-Sakash <edward.sakash@yahoo.com>
2023-04-25 14:55:50 +0200

    done

---
+++ task1.py
@@ -0,0 +1,35 @@
+
+"""
+name = "Mary"
+
+text = "Python is amazing!"
+
+print(name) # In this case I will print the content of the name variable
+"""
+
+"""# Task 1
+city = "London"
+print(city)"""
+
+"""
+# Task 2
+city = "Berlin"
+population = 3645000
+print(city, ":",population)
+"""
+
+"""# Task 3
+city = 'London'
+population = 9000000
+
+print(f'City: {city} ({isinstance(city, str)})')
+print(f'Population: {population}')
+"""
+
+
+"""# Task 4
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+if 'capital' in text:
+    print(f"capital: {text.index('capital')}")
+"""
